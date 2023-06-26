2bc6b74fb0f11d91ee75f93bfebf8653adc9325e
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2023-06-26 12:34:34 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+extern crate ethereum_bn128;
+extern crate rustc_hex;
+
+use rustc_hex::FromHex;
+use rustc_hex::ToHex;
+
+
+#[wasm_bindgen]
+pub fn ec_mul(input_hex: String) -> String {
+    let input_parsed = FromHex::from_hex(&*input_hex).unwrap();
+
+    let mut output = [0u8; 64];
+    match ethereum_bn128::bn128_mul(&input_parsed[..], &mut output) {
+        Ok(_) => {
+            let output_hex = output.to_hex();
+            return output_hex.into();
+        }
+        Err(_) => return "0x".into(),
+    }
+}
+
+#[wasm_bindgen]
+pub fn ec_add(input_str: String) -> String {
+    let input_parsed = FromHex::from_hex(&*input_str).unwrap();
+
+    let mut output = [0u8; 64];
+    match ethereum_bn128::bn128_add(&input_parsed[..], &mut output) {
+        Ok(_) => {
+            let output_hex = output.to_hex();
+            return output_hex.into();
+        }
+        Err(_) => return "0x".into(),
+    }
+}
+
+#[wasm_bindgen]
+pub fn ec_pairing(input_str: String) -> String {
+    let input_parsed = FromHex::from_hex(&*input_str).unwrap();
+
+    let mut output = [0u8; 32];
+    match ethereum_bn128::bn128_pairing(&input_parsed[..], &mut output) {
+        Ok(_) => {
+            let output_hex = output.to_hex();
+            return output_hex.into();
+        }
+        Err(_) => return "0x".into(),
+    }
+}
