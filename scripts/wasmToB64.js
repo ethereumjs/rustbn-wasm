448a304533815d8a0bcaa40fa414efbb5becdbd8
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-03-06 15:28:48 -0500

    More clean up

--- scripts/wasmToB64.js
+++ scripts/wasmToB64.js
@@ -1,5 +1,5 @@
 // Converts WASM bytecode to JSON object to create JSON that can be loaded in ESM module without requiring `fetch`
-const wasmBytes = require('fs').readFileSync('./dist/cjs/rustbn_bg.wasm')
+const wasmBytes = require('fs').readFileSync('./wasm/rustbn_bg.wasm')
 const encoded = Buffer.from(wasmBytes, 'binary').toString('base64');
 const json = {
     wasm: encoded
